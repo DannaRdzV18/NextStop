c83c1fe39b07beb0cabb6f99f992e545391978bb
me1anye <me1anye@users.noreply.github.com>
2025-11-03 21:19:25 -0600

    Se m odifico apps.js, para las nuevas interfaces

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Registro from './components/Registro';
 import Verificar from './components/Verificar';
 import Login from './components/Login';
+import VerificacionEnlace from './components/VerificacionEnlace'; // 👈 importar nuevo componente
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/registro" element={<Registro />} />
           <Route path="/verificar" element={<Verificar />} />
+          <Route path="/verificar-enlace" element={<VerificacionEnlace />} /> {/* 👈 nueva ruta */}
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
