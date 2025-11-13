ccc34b7b5c1367d2e0c91aab81b9c33e3f4bf46e
DrozxJs <maussbrian06@gmail.com>
2025-11-13 13:26:02 -0600

    Actualizaciones

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://nextstop-app-u9cvd.ondigitalocean.app/",
+  baseURL: "https://nextstop-app-u9cvd.ondigitalocean.app/api/",
 });
 
 export default API;
